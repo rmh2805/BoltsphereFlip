3523eab83bbf54d2b7a0c73d381784972e9b98fc
rmh2805 <rmh2805@g.rit.edu>
2020-09-04 22:58:34 -0400

    Stubbed out the curses display. Stubbed a curses display test program.

---
+++ cursesDisp.c
@@ -0,0 +1,26 @@
+#include "cursesDisp.h"
+
+int cursesDispInit() {
+    return EXIT_FAILURE;    //Stub this out for now
+}
+
+
+void cursesDispClose() {
+
+}
+
+void cursesDispBoard(board_t board) {
+
+}
+
+void cursesDispHelp(char help, char note, char flip, char quit) {
+
+}
+
+void cursesDispStatus(size_t score, size_t nRemain, const char * msg) {
+
+}
+
+void cursesGetCmd(char * buf, size_t bufSize) {
+
+}
---
+++ testCursesDisp.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "board.h"
+#include "cursesDisp.h"
+
+int main() {
+    //========================================<Allocation>========================================//
+    board_t board = makeBoardDef();
+    if(board == NULL) {
+        fprintf(stderr, "Failed to allocate the board, exit failure\n");
+        return EXIT_FAILURE;
+    }
+
+    if(cursesDispInit() != EXIT_SUCCESS) {
+        delBoard(board);
+        fprintf(stderr, "Failed to initialize the curses display, exit failure\n");
+        return EXIT_FAILURE;
+    }
+
+    srand((unsigned int) time(NULL));   //Initialize rng for board
+    randInit(board);    //Initialize the board with random scores
+
+    //========================================<Main Code>=========================================//
+
+    //=========================================<Cleanup>==========================================//
+    delBoard(board);
+    cursesDispClose();
+    return EXIT_SUCCESS;
+}
