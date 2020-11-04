4c0eccf92d20c45b5ded50a410cadc33a732d2ef
rmh2805 <rmh2805@g.rit.edu>
2020-11-04 14:06:36 -0500

    Updated display with printFrame function

--- cursesDisp.c
+++ cursesDisp.c
@@ -92,79 +92,7 @@ void cursesDispClose() {
     endwin();
 }
 
-void cDispCell(board_t board, size_t row, size_t col) {
-    //Calculate the palette for this card
-    bool flipped = isFlipped(board, row, col);
-    char notes = getNotes(board, row, col);
-    int score = getScore(board, row, col);    
-
-    int palette = kUnflippedPalette;
-    if(flipped) {
-        palette = (score) ? kFlippedPalette : kBombPalette;
-    }
-    
-    //Do screen space calculations
-    size_t screenCol = getCardCol(col, getNCols(board));
-    size_t screenRow = getCardRow(row, getNRows(board));
-
-    //Prepare the buffer
-    char buf[kCardWidth + 1];
-    buf[kCardWidth] = 0;
-
-    //Print the upper and lower borders
-    buf[0] = '+';
-    for(size_t col = 1; col < kCardWidth - 1; col++) {
-        buf[col] = (col == kCardWidth/2) ? '+' : '-';
-    }
-    buf[kCardWidth - 1] = '+';
-    
-    wattron(stdscr, COLOR_PAIR(palette));  
-    mvprintw(screenRow, screenCol, buf);
-    mvprintw(screenRow + kCardHeight - 1, screenCol, buf);
-
-    buf[0] = '|';
-    buf[kCardWidth-1] = '|';
-    for(size_t row = 1; row < kCardHeight - 1; row++) {
-        if(row == kCardHeight/2) {
-            buf[0] = '+';
-            buf[kCardWidth - 1] = '+';
-        }
-
-        for(size_t col = 1; col < kCardWidth - 1; col++) {
-            if(!flipped) {
-                if(row == kCardHeight/2) {
-                    buf[col] = (col == kCardWidth/2) ? '+' : '-';
-                } else if (row == kCardHeight / 4) {
-                    if(col == kCardWidth / 4) buf[col] = (notes & 0x01) ? '0' : ' ';
-                    else if(col == 3 * kCardWidth / 4) buf[col] = (notes & 0x02) ? '1' : ' ';
-                } else if (row == 3 * kCardHeight / 4) {
-                    if(col == kCardWidth / 4) buf[col] = (notes & 0x04) ? '2' : ' ';
-                    else if(col == 3 * kCardWidth / 4) buf[col] = (notes & 0x08) ? '3' : ' '; 
-                } else {
-                    buf[col] = ' ';
-                }
-                
-                if(col == kCardWidth/2) buf[col] = (row == kCardHeight/2) ? '+' : '|';
-
-            } else {
-                buf[col] = (col == kCardWidth/2 && row == kCardHeight/2) ? '0' + score : ' ';
-            }
-        }
-        
-        mvprintw(screenRow + row, screenCol, buf);
-        if(row == kCardHeight/2) {
-             buf[0] = '|';
-             buf[kCardWidth-1] = '|';
-        }
-
-
-    }
-
-
-    refresh();
-    wattroff(stdscr, COLOR_PAIR(palette));
-}
-
+void cDispCell(board_t board, size_t row, size_t col);
 void cDispRowIndics(board_t board, size_t row);
 void cDispColIndics(board_t board, size_t col);
 
@@ -207,11 +135,119 @@ void cursesGetCmd(char * buf, size_t bufSize) {
 }
 
 //=======================================<Helper Functions>=======================================//
-void cDispRowIndics(board_t board, size_t row) {
+void cDispFrame(size_t screenRow, size_t screenCol) {
+    char buf[kCardWidth + 1];
+    buf[kCardWidth] = 0;
+    buf[0] = '+';
+    buf[kCardWidth-1] = '+';
+    for(size_t col = 1; col < kCardWidth-1; col++) {
+        buf[col] = (col == kCardWidth/2) ? '+' : '-';
+    }
+    
+    mvprintw(screenRow, screenCol, buf);
+    mvprintw(screenRow + kCardHeight-1, screenCol, buf);
+    
+    buf[0] = '|';
+    buf[kCardWidth - 1] = '|';
+    for(size_t col = 1; col < kCardWidth-1; col++) {
+        buf[col] = ' ';
+    }
+    for(size_t row = 1; row < kCardHeight-1; row++) {
+        if(row == kCardHeight/2) {
+            buf[0] = '+';
+            buf[kCardWidth-1] = '+';
+        }
 
+        mvprintw(screenRow + row, screenCol, buf);
+
+        if(row == kCardHeight/2) {
+            buf[0] = '|';
+            buf[kCardWidth-1] = '|';
+        }
+    }
+
+}
+
+void cDispRowIndics(board_t board, size_t row) {
+    //Calculate the information for this row
+    size_t totScore = 0;
+    size_t nBombs = 0;
+    
+    size_t nCols = getNCols(board);
+
+    for(size_t col = 0; col < nCols; col++) {
+        size_t score = getScore(board, row, col);
+        totScore += score;
+        if(score == 0) ++nBombs;
+    }
+
+    //Do screen space calculations
+    size_t lCol = getCardCol(0, nCols) - kCardWidth - 1;
+    size_t rCol = getCardCol(nCols, nCols);
+    size_t screenRow = getCardRow(row, getNRows(board));
+
+    wattron(stdscr, COLOR_PAIR(kIndicatorPalette));
+
+    //Print the frames
+    cDispFrame(screenRow, lCol);
+    cDispFrame(screenRow, rCol);
+
+    //Print the row number in the left indicator
+    mvaddch(screenRow + kCardHeight/2, lCol + kCardWidth/2, '0' + row + 1);
+
+    refresh();
+    wattroff(stdscr, COLOR_PAIR(kIndicatorPalette));
 }
 
 void cDispColIndics(board_t board, size_t col) {
 
 }
 
+void cDispCell(board_t board, size_t row, size_t col) {
+    bool flipped = isFlipped(board, row, col);
+    char notes = getNotes(board, row, col);
+    int score = getScore(board, row, col);    
+
+    int palette = kUnflippedPalette;
+    if(flipped) {
+        palette = (score) ? kFlippedPalette : kBombPalette;
+    }
+    
+    //Do screen space calculations
+    size_t screenCol = getCardCol(col, getNCols(board));
+    size_t screenRow = getCardRow(row, getNRows(board));
+    
+    //Start printing
+    wattron(stdscr, COLOR_PAIR(palette));
+    
+    //Print the card frame  
+    cDispFrame(screenRow, screenCol);
+    
+    //Print the score or the notes
+    if(flipped) {
+        mvprintw(screenRow + kCardHeight/2, screenCol + kCardWidth/2, "%d", score);
+    } else {
+        //Print the card back
+        for(size_t row = 1; row < kCardHeight-1; row++) {
+            mvaddch(screenRow + row, screenCol + kCardWidth/2, '|');
+        }
+        
+        for(size_t col = 1; col < kCardWidth-1; col++) {
+            mvaddch(screenRow + kCardHeight/2, screenCol + col, (col == kCardWidth/2) ? '+' : '-');
+        }
+
+        //Print whichever notes are present
+        if(notes & 0x01) 
+            mvaddch(screenRow + kCardHeight/4, screenCol + kCardWidth/4, '0');
+        if(notes & 0x02)
+            mvaddch(screenRow + kCardHeight/4, screenCol + kCardWidth*3/4, '1');
+        if(notes & 0x04)
+            mvaddch(screenRow + kCardHeight*3/4, screenCol + kCardWidth/4, '2');
+        if(notes & 0x08)
+            mvaddch(screenRow + kCardHeight*3/4, screenCol + kCardWidth*3/4, '3');
+
+    }
+
+    refresh();
+    wattroff(stdscr, COLOR_PAIR(palette));
+}
