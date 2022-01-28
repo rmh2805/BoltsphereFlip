28a9adb93acca9e1a98c4d5f238eea81efdd45f8
Ray Healy <rmh2805@rit.edu>
2022-01-28 10:03:47 -0500

    Made the new printDisplay

--- printDisp.c
+++ printDisp.c
@@ -1,198 +1,92 @@
 #include "printDisp.h"
 
+//============================<Declare Disp Impls>============================//
+int     print_init         (void** data);
+int     print_close        (void* data);
+void    print_printText    (void * data, const char * str, short palette, size_t row, size_t col);
+int     print_printBuf     (void * data, dispBuf_t buf);
+void    print_clearScr     (void* data);
+int     print_getCh        (void* data);
+void    print_getStr       (void* data, char* buf, size_t nBuf);
+void    print_getScrSize   (void* data, size_t* rows, size_t* cols);
 
-#define kHoriBorder "+===+"
-#define kHoriSpacer "  |  "
+//============================<Exposed Functions>=============================//
+disp_t mkCursesDisp() {
+    disp_t disp = mkNullDisp();
 
-#define kNoteFormat "|%c|%c|"
-#define kMiddleFormat "|-+-|"
+    disp.init = print_init;
+    disp.close = print_close;
+    disp.printText = print_printText;
+    disp.printBuf = print_printBuf;
+    disp.clearScr = print_clearScr;
+    disp.getCh = print_getCh;
+    disp.getStr = print_getStr;
+    disp.getScrSize = print_getScrSize;
 
-#define kEmptyFormat "|   |"
-#define kPointFormat "| %c |" 
+    disp.monoDisp = true;
+    disp.data = NULL;
 
-#define kBottomPointFormat "|%2d |"
-
-#define kLBorder "==+"
-#define kLIndicator " %c|"
-#define kLBlank "  |"
-#define kIndent "   "
-
-#define kRBorder "+=="
-#define kRIndicator "|%2d"
-#define kRBlank "|  "
-
-int printDispInit(board_t board) {
-    return EXIT_SUCCESS;
+    return disp;
 }
 
-void printDispClose() {
+//================================<Disp Impls>================================//
+int print_init (void** data){
+    data = data;
+    return 0;
+}
 
-} 
+int print_close (void* data){
+    data = data;
+    return 0;
+}
 
-void printDispRow(board_t board, size_t row) {
-    int totScore = 0;
-    int totBombs = 0;
-    
-    for(size_t col = 0; col < getNCols(board); col++) {
-        int score = getScore(board, row, col);
-        totScore += score;
-        totBombs += (score == 0) ? 1 : 0;
-    }
+void print_printText (void * data, const char * str, short palette, size_t row, size_t col) {
+    data = data;
+    palette = palette;
+    row = row;
 
-    printf(kIndent);
-    //Print a spacer from the line above
-    for(size_t col = 0; col < getNCols(board); col++) {
+    for(size_t i = 0; i < col; ++i) {
         printf(" ");
-        printf(kHoriSpacer);
     }
-    printf("\n");
-    
-    printf(kLBorder);
-    //Print the upper border of the card
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf(" ");
-        printf(kHoriBorder);
-    }
-    printf(" ");
-    printf(kRBorder);
-    printf("\n");
-    
-    printf(kLBlank);
-    //If not flipped, print top row of notes, else print a blank line
-    for(size_t col = 0; col < getNCols(board); col++) {
-            printf(" ");
-        if(isFlipped(board, row, col)) {
-            printf(kEmptyFormat);
-        } else {
-            char notes = getNotes(board, row, col);
-            printf(kNoteFormat, (notes & 0x1) ? '0' : ' ', 
-                                (notes & 0x2) ? '1' : ' ');
+    printf("%s\n", str);
+}
+
+int print_printBuf (void * data, dispBuf_t buf){
+    if(buf.data == NULL) return -1;
+    data = data;
+
+    for(size_t row = 0; row < buf.screenRows; ++row) {
+        for(size_t col = 0; col < buf.screenCols; ++col) {
+            printf("%c", buf.data[row][col].ch);
         }
+        printf("\n");
     }
-    printf(" ");
-    printf(kRIndicator, totScore);
-    printf("\n");
-    
-    printf(kLIndicator, (char)('1' + row));
-    //If flipped, print point value, else print centerline
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf("=");
-        if(isFlipped(board, row, col)){
-            printf(kPointFormat, (char) ('0' + getScore(board, row, col)));
-        } else {
-            printf(kMiddleFormat);
-        }
-    }
-    printf("=");
-    printf(kRBlank);
-    printf("\n");
-    
-    printf(kLBlank);
-    //If not flipped, print bottom row of notes, else blank
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf(" ");
-        if(isFlipped(board, row, col)) {
-                printf(kEmptyFormat);
-        } else {
-            char notes = getNotes(board, row, col);
-            printf(kNoteFormat, (notes & 0x4) ? '2' : ' ', 
-                                (notes & 0x8) ? '3' : ' ');
-        }
-    }
-    printf(" ");
-    printf(kRIndicator, totBombs);
-    printf("\n");
-    
-    printf(kLBorder);
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf(" ");
-        printf(kHoriBorder);
-    }
-    printf(" ");
-    printf(kRBorder);
-    printf("\n");
-    
+    return 0;
 }
 
-void printDispBoard(board_t board) {
-    if(board == NULL)
-        return;
-    
-    printf(kIndent);
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf(" ");
-        printf(kPointFormat, (char)('A' + col));
+void print_clearScr (void* data){
+    data = data;
+
+    for(int i = 0; i < kDefPrintDisplyRows; ++i) {
+        printf("\n");
     }
-    printf("\n");
-    
-    printf(kIndent);
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf(" ");
-        printf(kHoriBorder);
-    }
-    printf("\n");
-    
-    
-    for(size_t row = 0; row < getNRows(board); row++) {
-        printDispRow(board, row);
-    }    
-    
-    printf(kIndent);
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf(" ");
-        printf(kHoriSpacer);
-    }
-    printf("\n");
-    
-    printf(kIndent);
-    for(size_t col = 0; col < getNCols(board); col++) {
-        printf(" ");
-        printf(kHoriBorder);
-    }
-    printf("\n");
-    
-    printf(kIndent);
-    for(size_t col = 0; col < getNCols(board); col++) {
-        int tot = 0;
-        for(size_t row = 0; row < getNRows(board); row++) 
-            tot += getScore(board, row, col);
-        
-        printf(" ");
-        printf(kBottomPointFormat, tot%100);
-    }
-    printf("\n");
-    
-    printf(kIndent);
-    for(size_t col = 0; col < getNCols(board); col++) {
-        int tot = 0;
-        for(size_t row = 0; row < getNRows(board); row++) 
-            tot += (getScore(board, row, col) == 0) ? 1 : 0;
-        
-        printf(" ");
-        printf(kBottomPointFormat, tot%100);
-    }
-    printf("\n");
-    
-    
 }
 
-void printDispHelp(char help, char note, char flip, char quit) {
-    printf("%c: Print this menu\n", help);
-    printf("%c: Quit the game\n", quit);
-    printf("%c[col][row][value(s)]: Mark the specified card with the specified values (0-3)\n", note);
-    printf("%c[col][row]: Flip the specified card\n", flip);
-    fgetc(stdin);
+int print_getCh (void* data){
+    data = data;
+    return getchar();
 }
 
-void printDispStatus(size_t score, size_t nRemain, const char * msg) {
-    printf("\n===================================================================\n");
-    printf("Score: %lu\n", score);
-    printf("Remaining Required Cards: %lu\n", nRemain);
-    printf("%s\n", msg);
-    printf("\n");
+void print_getStr (void* data, char* buf, size_t nBuf){
+    if(buf == NULL || nBuf == 0) return;
+    data = data;
+
+    fgets(buf, nBuf, stdin);
 }
 
-void printGetCmd(char * buf, size_t bufSize) {
-    printf("\nEnter your command: ");
-    fgets(buf, bufSize, stdin);
+void print_getScrSize (void* data, size_t* rows, size_t* cols){
+    data = data;
+
+    *rows = kDefPrintDisplyRows;
+    *cols = kDefPrintDisplayCols;
 }
--- printDisp.h
+++ printDisp.h
@@ -1,39 +1,21 @@
-#ifndef  _PRINT_DISP_H_
-#define  _PRINT_DISP_H_
+#ifndef _PRINT_DISP_H_
+#define _PRINT_DISP_H_
 
 #include <stdlib.h>
+#include <stdbool.h>
+
 #include <stdio.h>
 
-#include "board.h"
+#include "dispBase.h"
+
+#define kDefPrintDisplyRows 52
+#define kDefPrintDisplayCols 80
 
 /**
- * Provided for parity with other display code, always returns success
+ * @brief Generates a stdio based display interface
+ * 
+ * @return A stdio based display interface
  */
-int printDispInit(board_t board);
+disp_t mkPrintDisp();
 
-/**
- * Provided for parity with other display code
- */
-void printDispClose();
-
-/**
- * Prints the board to stdout
- */
-void printDispBoard(board_t board);
-
-/**
- * Prints a help message and then halts for input
- */
-void printDispHelp(char help, char note, char flip, char quit);
-
-/**
- * Prints a status indicator 
- */
-void printDispStatus(size_t score, size_t nRemain, const char * msg);
-
-/**
- * Gets a command from stdin, puts it into buf
- */
-void printGetCmd(char * buf, size_t bufSize);
-
-#endif //_PRINT_DISP_H_
+#endif
